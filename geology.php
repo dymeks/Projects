39ddd88ccb2d335cfc8c220348de55b32de7afab
susie <scdymek@msn.com>
2015-12-20 22:33:27 -0800

    version1

---
+++ geology.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<head> 
+	<title>Rocks and Minerals</title>
+	<link href="geology.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+	<h1>Rocks and Minerals!</h1>
+	<div>
+	  <img src = "" alt= "mineralPicture">
+	</div>
+	<div>
+	  <input type="text" name="mineralName">
+	  <input type="submit" name="mineralName">
+	</div>  
+</body>
